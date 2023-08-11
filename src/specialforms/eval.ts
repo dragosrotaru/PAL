93fd3adc53c5f1ae777fd676e10ecccd506be82b
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-08-11 18:41:16 +0200

    added infinite recursion check in eval

--- src/specialforms/eval.ts
+++ src/specialforms/eval.ts
@@ -9,5 +9,10 @@ export const Identifier = Symbol.for("eval");
 export const Is = (ast: Lang.AST): ast is Form =>
   STATIC.IsList(ast) && ast.length === 2 && ast[0] === Identifier;
 
-export const Apply = (ctx: IContext) => async (ast: Form) =>
-  ctx.eval(ctx)(await ctx.eval(ctx)(ast[1]));
+export const Apply = (ctx: IContext) => async (ast: Form) => {
+  const next = await ctx.eval(ctx)(ast[1]);
+  if (ctx.ts.valueEquals(ast[1], next)) {
+    return next;
+  }
+  return ctx.eval(ctx)(next);
+};
